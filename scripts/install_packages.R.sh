2da1c56ae1f0f3f2a725e938dc426629d76979e4
heroalone <heroalone@qq.com>
2023-08-17 17:14:45 +0200

    update R package install script

--- scripts/install_packages.R.sh
+++ scripts/install_packages.R.sh
@@ -1,28 +1,31 @@
 cat("Loading R libraries\n")
 
 if(!suppressMessages(library(genio, warn.conflicts=F))){
-    install.packages("genio", dep=T)
-    if(!suppressMessages(library(genio, warn.conflicts=F))) {
-        stop('Install genio R package failed! You may have to install it manually.\n')
-    }
+	install.packages("genio", dep=T)
+	if(!suppressMessages(library(genio, warn.conflicts=F))) {
+		stop("Install genio R package failed! You may have to install it manually.\n")
+	}
 }
 
 
 if(!suppressMessages(library(ranger, warn.conflicts=F))){
-    install.packages("ranger", dep=T)
-    if(!suppressMessages(library(ranger, warn.conflicts=F))) {
-        stop('Install ranger R package failed! You may have to install it manually.\n')
-    }
+	install.packages("ranger", dep=T)
+	if(!suppressMessages(library(ranger, warn.conflicts=F))) {
+		stop("Install ranger R package failed! You may have to install it manually.\n")
+	}
 }
 
 
 if(!suppressMessages(library(logicFS, warn.conflicts=F))){
-    install.packages("logicFS", dep=T)
-    if(!suppressMessages(library(logicFS, warn.conflicts=F))) {
-        stop('Loading package cowplot failed! Please install manually.\n')
-    }
-}
+	if (!require("BiocManager", quietly = TRUE))
+		install.packages("BiocManager")
 
+	BiocManager::install("logicFS")
+
+	if(!suppressMessages(library(logicFS, warn.conflicts=F))) {
+		stop("Loading package cowplot failed! Please install manually.\n")
+	}
+}
 
 cat("R packages installed succesfully\n")
 
