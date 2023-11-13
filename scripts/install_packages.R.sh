7dd66af3a12bdbd345b9b9df73ceaf317cb96ccf
heroalone <heroalone@qq.com>
2023-11-13 16:34:38 +0100

    private scripts

--- scripts/install_packages.R.sh
+++ scripts/install_packages.R.sh
@@ -1,19 +1,19 @@
 cat("Installing R libraries\n")
 
 install_package <- function(package_name) {
-  if (!require(package_name, character.only = TRUE, quietly = TRUE)) {
-    install.packages(package_name, dependencies = TRUE)
-    if (!require(package_name, character.only = TRUE, quietly = TRUE)) {
-      stop(paste("Install", package_name, "R package failed! You may have to install it manually.\n"))
-    }
-  }
+	if (!require(package_name, character.only = TRUE, quietly = TRUE)) {
+		install.packages(package_name, dependencies = TRUE)
+		if (!require(package_name, character.only = TRUE, quietly = TRUE)) {
+			stop(paste("Install", package_name, "R package failed! You may have to install it manually.\n"))
+		}
+	}
 }
 
 install_package("genio")
 install_package("ranger")
 
 if (!requireNamespace("BiocManager", quietly = TRUE)) {
-  install.packages("BiocManager")
+	install.packages("BiocManager")
 }
 BiocManager::install("logicFS")
 
@@ -22,17 +22,17 @@ BiocManager::install("logicFS")
 required_packages <- c("genio", "ranger", "logicFS")
 
 recheck_packages <- function(package_names) {
-  missing_packages <- package_names[!sapply(package_names, requireNamespace, quietly = TRUE)]
-  return(missing_packages)
+	missing_packages <- package_names[!sapply(package_names, requireNamespace, quietly = TRUE)]
+	return(missing_packages)
 }
 
 
 missing_packages <- recheck_packages(required_packages)
 
 if (length(missing_packages) == 0) {
-  cat("Required R packages have been successfully installed!\n")
+	cat("Required R packages have been successfully installed!\n")
 } else {
-  cat("The installation of the following packages has failed. Please install them manually.\n")
-  cat(paste(missing_packages, collapse = ", "), "\n")
+	cat("The installation of the following packages has failed. Please install them manually.\n")
+	cat(paste(missing_packages, collapse = ", "), "\n")
 }
 
